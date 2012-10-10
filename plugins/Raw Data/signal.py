c43bf8d61f5d72be2d81ed71ef93fe979ecceef3
Robert <robert.proepper@googlemail.com>
2012-10-11 01:36:51 +0200

    Improved options for Signal Plot plugin

--- plugins/Raw Data/signal.py
+++ plugins/Raw Data/signal.py
@@ -2,8 +2,9 @@ from spykeutils.plugin import analysis_plugin, gui_data
 from spykeutils import SpykeException
 import spykeutils.conversions as convert
 from spykeutils import plot
-import neo
 from copy import copy
+
+spike_prop = gui_data.ValueProp(False)
 
 class SignalPlotPlugin(analysis_plugin.AnalysisPlugin):
     """ Signal Plot
@@ -15,13 +16,18 @@ class SignalPlotPlugin(analysis_plugin.AnalysisPlugin):
                                         default=2)
     show_events = gui_data.BoolItem('Show events', default=True)
     show_epochs = gui_data.BoolItem('Show epochs', default=True)
-    show_waveforms = gui_data.BoolItem('Show spike waveforms', 
-                                       default=False)
-    template_mode = gui_data.BoolItem('Use first spike as template')
-    st_mode = gui_data.ChoiceItem('Spike Trains', 
-                                  ('Not used', 'Show spike events',
-                                   'Show spike waveforms'),
-                                  default=2)
+    
+    _g = gui_data.BeginGroup('Spikes')
+    show_spikes = gui_data.BoolItem(
+        'Show spikes').set_prop('display', store=spike_prop)
+    spike_form = gui_data.ChoiceItem('Display as',
+        ('Waveforms', 'Lines')).set_prop('display', active=spike_prop)
+    spike_mode = gui_data.ChoiceItem('Included data', 
+        ('Spikes', 'Spike Trains', 'Both'), 
+        default=2).set_prop('display', active=spike_prop)
+    template_mode = gui_data.BoolItem(
+        'Use first spike as template').set_prop('display', active=spike_prop)
+    _g_ = gui_data.EndGroup('Spikes')
     
     def get_name(self):
         return 'Signal Plot'
@@ -52,12 +58,12 @@ class SignalPlotPlugin(analysis_plugin.AnalysisPlugin):
             epochs = current.epochs()
 
         spike_trains = None
-        if self.st_mode > 0:
+        if self.show_spikes and self.spike_mode > 0:
             current.progress.set_status('Loading spike trains')
             spike_trains = current.spike_trains_by_segment()
 
         spikes = None
-        if self.show_waveforms:
+        if self.show_spikes and self.spike_mode != 1:
             current.progress.set_status('Loading spikes')
             spikes = current.spikes_by_segment()
 
@@ -93,31 +99,31 @@ class SignalPlotPlugin(analysis_plugin.AnalysisPlugin):
                     seg_signals.extend(
                         convert.analog_signal_array_to_analog_signals(sa))
             
-            # Prepare Spike waveforms
-            if self.st_mode==2 and seg_trains:
+            # Prepare template spikes
+            if self.spike_form == 0 and self.template_mode:
                 if seg_spikes is None:
                     seg_spikes = []
 
                 template_spikes = {}
-                if self.template_mode:
-                    for s in seg_spikes:
-                        if s.unit not in template_spikes:
-                            template_spikes[s.unit] = s
+                for s in seg_spikes:
+                    if s.unit not in template_spikes:
+                        template_spikes[s.unit] = s
+                for ts in template_spikes.itervalues():
+                    seg_spikes.remove(ts)
                     
-                for st in seg_trains:
-                    if st.waveforms is not None:
-                        seg_spikes.extend(
-                            convert.spike_train_to_spikes(st))
-                    elif st.unit in template_spikes:
-                        for t in st:
-                            s = copy(template_spikes[st.unit])
-                            s.time = t
-                            seg_spikes.append(s)
-                seg_trains = None
-                    
+                for st in seg_trains[:]:
+                    if st.unit not in template_spikes:
+                        continue
+                    for t in st:
+                        s = copy(template_spikes[st.unit])
+                        s.time = t
+                        seg_spikes.append(s)
+                    seg_trains.remove(st)
+
             plot.signals(seg_signals, events=seg_events, 
                          epochs=seg_epochs, spike_trains=seg_trains,
-                         spikes=seg_spikes, use_subplots=self.subplots,
+                         spikes=seg_spikes, use_subplots=self.subplots, 
+                         show_waveforms=(self.spike_form==0),
                          progress=current.progress)
             
             # Only do one plot
