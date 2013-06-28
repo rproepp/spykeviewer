6349371cdd7271247aa557dbcfa1bae252d41aea
Robert Pröpper <robert.proepper@gmail.com>
2013-06-28 17:31:39 +0200

    Correlograms can be per second or per segment

--- spykeviewer/plugins/Spike Trains/correlogram.py
+++ spykeviewer/plugins/Spike Trains/correlogram.py
@@ -8,6 +8,7 @@ class CorrelogramPlugin(analysis_plugin.AnalysisPlugin):
     bin_size = gui_data.FloatItem('Bin size', 1.0, 0.1, 10000.0, unit='ms')
     cut_off = gui_data.FloatItem('Cut off', 50.0, 2.0, 10000.0, unit='ms')
     data_source = gui_data.ChoiceItem('Data source', ('Units', 'Selections'))
+    count_per = gui_data.ChoiceItem('Counts per', ('Second', 'Segment'))
     border_correction = gui_data.BoolItem('Border correction', default=True)
 
     def get_name(self):
@@ -26,6 +27,7 @@ class CorrelogramPlugin(analysis_plugin.AnalysisPlugin):
 
         plot.cross_correlogram(d, self.bin_size*pq.ms,
             self.cut_off*pq.ms, self.border_correction,
+            self.count_per == 0,
             progress=current.progress)
 
 
